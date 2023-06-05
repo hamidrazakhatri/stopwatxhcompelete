3e1e71d65f8a08d58fac5884abad3117068dd852
mohtashimkhatri <128294671+mohtashimkhatri@users.noreply.github.com>
2023-06-05 04:50:00 -0700

    complete stopwatch

---
+++ app.js
@@ -0,0 +1,35 @@
+var [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+var timerRef = document.querySelector(".timer-display");
+var int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 10);
+});
+
+function displayTimer() {
+  milliseconds += 10;
+  seconds = milliseconds == 1000 ? (seconds + 1) % 60 : seconds;
+  minutes = seconds == 0 && milliseconds == 0 ? (minutes + 1) % 60 : minutes;
+  hours = minutes == 0 && seconds == 0 && milliseconds == 0 ? hours + 1 : hours;
+  milliseconds = milliseconds == 1000 ? 0 : milliseconds;
+
+ var h = String(hours).padStart(2, "0");
+  var m = String(minutes).padStart(2, "0");
+  var s = String(seconds).padStart(2, "0");
+  var ms = String(milliseconds).padStart(3, "0");
+
+  timerRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+  clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+  clearInterval(int);
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerRef.innerHTML = "00 : 00 : 00 : 000";
+});
